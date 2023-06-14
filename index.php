63bfada6971cc62e4536a36e2e33a556b3fcc161
mfatihaa <fatihjr1011@gmail.com>
2023-06-14 19:21:22 +0700

    Update : 14/06/2023

--- index.php
+++ index.php
@@ -112,10 +112,11 @@ if (!isset($_SESSION['id_pelanggan']) && $_SESSION['username']) {
     </div>
 
     <!-- Whatsapp Chat -->
-    <div class="container p-4 d-block position-absolute">
-        <button class="btn btn-primary shadow-none">
+    <div class="container p-4 d-block">
+        <button class="btn btn-success shadow-none position-absolute">
             <a href="https://wa.me/6289677808322?text=Assalamualaikum" class="text-decoration-none text-white">
-                <i class='bx bxl-whatsapp'></i> Whatsapp Admin
+                <i class='bx bxl-whatsapp'></i>
+                <p>Whatsapp Admin</p>
             </a>
         </button>
     </div>
