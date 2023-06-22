d7d001c5ac4225d3b963f5a64e248e3275ce6c45
aknaufal00 <akbar@student.jgu.ac.id>
2023-06-22 16:03:58 +0700

    Update : 22/06/2023

--- buy-ing.php
+++ buy-ing.php
@@ -13,7 +13,5 @@ if (isset($_SESSION['cart'][$id_product])) {
 else {
     $_SESSION['cart'][$id_product] = 1;
 }
-echo "<pre>";
-print_r($_SESSION['cart']);
-echo "</pre>";
 echo "<script>alert('Produk/Service telah masuk ke keranjang');</script>";
+header("Location: ./cart");
